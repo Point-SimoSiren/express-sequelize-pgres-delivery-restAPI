a4b4446e36a70309d8fb2d5231d285fac682e213
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-06-14 00:07:51 +0300

    raw query fine and param checked for sql inj

--- controllers/order-row.js
+++ controllers/order-row.js
@@ -1,5 +1,6 @@
 const orderRowRouter = require('express').Router()
 const { Orderrow } = require('../models')
+const db = require('../models/index')
 
 //const jwt = require('jsonwebtoken')
 
@@ -9,29 +10,32 @@ orderRowRouter.get('/', async (req, res) => {
     res.json(orderRows)
 })
 
-/* TO DO NEXT RAW:
-GET ORDER-ROWS BY ORDER_ID PLUS SHOW ITEM´S PRODUCTNAME
-SELECT * FROM orderrow LEFT JOIN item
-ON item.item_id = orderrow.item_id WHERE order_id = '9fb9d681-7555-4e6c-a92b-921aa008fc19'
-*/
-
-
-/*
-// GET ORDER-ROWS BY ORDER_ID
+// RAW SQL TO QUERY: ROWS BY ORDER_ID WITH JOINED ITEM TABLE TO GET PRODUCTNAME
 orderRowRouter.get('/order/:orderid', async (req, res) => {
+
     try {
-        const rowsByOrder = await Orderrow.findAll({
-            where: {
-                order_id: req.params.orderid
-            }
-        })
-        return res.json(rowsByOrder)
+        const orderid = req.params.orderid
+
+        // Check the given parameter to avoid SQL injection
+        if (orderid.length < 33 || orderid.length > 39 ||
+            orderid.includes(";") || orderid.toUpperCase().includes("DROP") ||
+            orderid.toUpperCase().includes("DATA") ||
+            orderid.toUpperCase().includes("TABLE")) {
+
+            return res.status(400).end("Malformatted order_id")
+        }
+        const [results, metadata] = await db.sequelize.query
+            ("SELECT * FROM orderrow LEFT JOIN item ON item.item_id = orderrow.item_id WHERE  order_id = " + "'" + orderid + "'")
+
+        console.log(results)
+        return res.json(results)
     }
     catch (exception) {
+        console.log(exception)
         res.json(exception)
     }
 })
-*/
+
 
 // ADD NEW ROW TO ORDER
 orderRowRouter.post('/', async (req, res) => {
